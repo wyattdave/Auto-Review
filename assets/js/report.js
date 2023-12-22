2d1367940de549fc40bf9028b8d7b0a578fa097c
David Wyatt <davedidisco@gmail.com>
2023-12-22 10:24:10 +0000

    replace report to fix jump to top

--- assets/js/report.js
+++ assets/js/report.js
@@ -8,10 +8,36 @@ const inputSelectElem = document.getElementById("inputSelect");
 const downloadElem = document.getElementById("download");
 
 
+
 const actionIElem = document.getElementById("actionInput");
 const apiElm = document.getElementById("apiInput");
 const varElem = document.getElementById("variablesInput");
 const inputElem = document.getElementById("inputInput");
+
+
+downloadElem.addEventListener("click", function() {
+  downloadHTML()
+});
+
+function downloadHTML(){
+
+  let sHTML=document.querySelector("html").innerHTML;
+  let sName=document.getElementById("target-flowName").innerHTML;
+  sHTML='<!doctype html><html lang="en">'+sHTML+'</html>';
+  sHTML=sHTML.replace('<div class="mui--text-white mui--text-body2" style="font-size:10px" id="download">Download</div>','');
+  sHTML=sHTML.replaceAll('assets/','https://wyattdave.github.io/Auto-Review/assets/')
+  sHTML=sHTML.replaceAll('mu/js/mui.min.js','https://wyattdave.github.io/Auto-Review/mu/js/mui.min.js')
+
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+  encodeURIComponent(sHTML));
+  element.setAttribute('download', sName+'.html');
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+
+}
 
 actionSelectElem.addEventListener("change", function() {
   actionIElem.value="";
@@ -47,10 +73,12 @@ inputElem.addEventListener("change", function() {
   filterActionTable("inputTable","inputInput",inputSelectElem.value);
 });
 
-
-
-
 const startClear = document.getElementById("restScroll");
+startClear.addEventListener("click", function(e) {
+  e.stopPropagation();
+  topFunction()
+});
+
 startClear.click();
 
 const variablesElem = document.getElementById("clickVariables");
@@ -77,7 +105,6 @@ inpElem.addEventListener("click", function(e) {
   console.log(11)
   showComments("divInput");
 });
-
 
 const exceptElem = document.getElementById("clickExcept");
 exceptElem.addEventListener("click", function(e) {
@@ -122,4 +149,9 @@ function filterActionTable(sTableID,sInputID,col) {
       }   
     }    
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
